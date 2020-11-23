91826553cfa99057562bc183f17f63535b5c3556
Víctor Quirino <victorsouzaq@gmail.com>
2020-11-23 11:39:27 -0300

    Correção: Melhor tratamento para promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,11 @@ public class Program {
 				
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion(B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();//Converte a letra que o usuário digitar para maiúscula
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion(B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
